9e50b0bb2052060dea32092b8fa4f335bf9c1ad3
Ziad Abouelfarah <zain.fake02@gmail.com>
2024-03-17 13:55:26 +0000

    feat: update dependencies

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  googleClientId: process.env["GOOGLE_CLIENT_ID"],
-  serverUrl: process.env["SERVER_URL"],
+  googleClientId: process.env["GOOGLE_CLIENT_ID"]!,
+  serverUrl: process.env["SERVER_URL"]!,
 };
